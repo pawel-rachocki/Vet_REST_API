d83b252296637baa38ea01aded39d4680f166171
Pawel <pawel.logo@gmail.com>
2025-04-27 22:48:39 +0200

    Added get animals/animal endpoints

--- Vet-REST-API/Controllers/AnimalsController.cs
+++ Vet-REST-API/Controllers/AnimalsController.cs
@@ -1,10 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Vet_REST_API.Models;
 
 namespace Vet_REST_API.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class AnimalsController
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
 {
-    
+    //Get all Animals
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> GetAnimals()
+    {
+        var animals = Database.GetAnimals();
+        return Ok(animals);
+    }
+
+    //Get Animal by id
+    [HttpGet("{id:int}")]
+    public ActionResult<Animal> GetAnimal(int id)
+    {
+        var animal = Database.GetAnimals().FirstOrDefault(x => x.Id == id);
+        if (animal == null)
+        {
+            return NotFound("Animal with given id does not exist");
+        }
+        return Ok(animal);
+    }
+
 }
